d5097d95c06b105449dbc0f4d2239e757c56ebe1
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-02-27 12:00:58 +0300

    still not done

--- task-2-vector/vector-int.c
+++ task-2-vector/vector-int.c
@@ -1,22 +1,34 @@
+/*
+
+   _____     __      __       _             
+  / ____|    \ \    / /      | |            
+ | |  __ _____\ \  / /__  ___| |_ ___  _ __ 
+ | | |_ |______\ \/ / _ \/ __| __/ _ \| '__|
+ | |__| |       \  /  __/ (__| || (_) | |   
+  \_____|        \/ \___|\___|\__\___/|_|   
+                                            
+                                            
+    Grindelf-Vector. An integer-storing vector data structure source-code file.
+    Created by GrindelfP on 2025-02-24. 
+
+*/
 #include "vector-type.h"
 #include "vector-int.h"
-
 
 IVectorInt createVectorInt(int size) {
 
     VectorInt* vector = (VectorInt*)malloc(sizeof(VectorInt));
 
-    if (size > 0) {
+    if (size > 0 || size == 0) {
         vector->buffer = (int*)malloc(size * sizeof(int));
         memset(vector->buffer, 0, size);
         vector->capacity = size;
         vector->count = size;
     } else {
-        ZERO_SIZE_MESSAGE;
+        WRONG_SIZE_MESSAGE;
     }
 
     return vector;
-
 }
 
 int* getDataVectorInt(IVectorInt vector) {
@@ -24,38 +36,62 @@ int* getDataVectorInt(IVectorInt vector) {
     VectorInt* vectorPointer = (VectorInt*)vector;
 
     return vectorPointer->buffer;
-
 }
 
 void resizeVectorInt(IVectorInt vector) {
 
     VectorInt* vectorPointer = (VectorInt*)vector;
 
-    vectorPointer->capacity = vectorPointer->capacity * 2;
+    if (vectorPointer->capacity == 0) vectorPointer->capacity = 1;
+    else vectorPointer->capacity = vectorPointer->capacity * 2;
+    
     vectorPointer->buffer = realloc(vectorPointer->buffer, vectorPointer->capacity);
-
+    
+    for (size_t i = vectorPointer->capacity / 2; i < vectorPointer->capacity; ++i) {
+        memset(vectorPointer->buffer[i], 0, sizeof(int));
+    }
 }
 
 void appendVectorInt(IVectorInt vector, int value) {
 
+    VectorInt* vectorPointer = (VectorInt*)vector;
 
-
+    if (vectorPointer->capacity == vectorPointer->count) {
+        resizeVectorInt(vector);
+        vectorPointer = (VectorInt*)vector;
+    } 
+    vectorPointer->count++;
+    memset(vectorPointer->buffer[vectorPointer->count], value, sizeof(int));
 }
 
-void disposeVectorInt(IVectorInt vector) { // IS IT OKAY?
+void disposeVectorInt(IVectorInt vector) {
 
     VectorInt* vectorPointer = (VectorInt*)vector;
 
+    for (size_t i = 0; i < vectorPointer->capacity; ++i) {
+        free(vectorPointer->buffer[i]);
+    }
     free(vectorPointer->buffer);
     free(vectorPointer);
-
 }
 
 void insertVectorInt(IVectorInt vector, int position, int value) {
 
     VectorInt* vectorPointer = (VectorInt*)vector;
 
-    // vectorPointer->buffer = memmove()
+    if (vectorPointer->capacity == vectorPointer->count) {
+        resizeVectorInt(vector);
+        vectorPointer = (VectorInt*)vector;
+    }
 
+    while (position <= vectorPointer->capacity) {
+        resizeVectorInt(vector);
+        vectorPointer = (VectorInt*)vector;
+    }
+    
+    vectorPointer->count = vectorPointer->capacity;
+    vectorPointer->buffer[position];
+
+    
 }
 
--- task-2-vector/vector-int.h
+++ task-2-vector/vector-int.h
@@ -1,3 +1,17 @@
+/*
+
+   _____     __      __       _             
+  / ____|    \ \    / /      | |            
+ | |  __ _____\ \  / /__  ___| |_ ___  _ __ 
+ | | |_ |______\ \/ / _ \/ __| __/ _ \| '__|
+ | |__| |       \  /  __/ (__| || (_) | |   
+  \_____|        \/ \___|\___|\__\___/|_|   
+                                            
+                                            
+    Grindelf-Vector. An integer-storing vector data structure header file.
+    Created by GrindelfP on 2025-02-24. 
+
+*/
 #ifndef vector_int
 #define vector_int
 
@@ -6,19 +20,55 @@
 #include <stdio.h>
 
 #define IVectorInt void*
-#define ZERO_SIZE_MESSAGE printf("%s\n", "Vector not initialized, size must be greater than 0!")
+#define WRONG_SIZE_MESSAGE printf("%s\n", "Vector not initialized, size must be greater of equal than 0!")
 
+/**
+ * Initialize vector.
+ * 
+ * @param size size of the required vector.
+ * 
+ * @return IVectorInt pointer to the initialized vector.
+ */
 IVectorInt createVectorInt(int size);
 
+/**
+ * Gets data from the vector.
+ * 
+ * @param vector IVectorInt pointer to the vector.
+ * 
+ * @return integer pointer to the data.
+ */
 int* getDataVectorInt(IVectorInt vector);
 
+/**
+ * Resizes vector to fit in more data.
+ * 
+ * @param vector IVectorInt pointer to the vector.
+ */
 void resizeVectorInt(IVectorInt vector);
 
+/**
+ * Add data to the end of the filled part of the vector.
+ * 
+ * @param vector IVectorInt pointer to the vector.
+ * @param value value element.
+ */
 void appendVectorInt(IVectorInt vector, int value);
 
+/**
+ * Deletes vector.
+ * 
+ * @param vector IVectorInt pointer to the vector.
+ */
 void disposeVectorInt(IVectorInt vector);
 
+/**
+ * Inserts data into the vector at particular index.
+ * 
+ * @param vector IVectorInt pointer to the vector.
+ * @param position required index of the new added element.
+ * @param value value element. 
+ */
 void insertVectorInt(IVectorInt vector, int position, int value);
-
 
 #endif
