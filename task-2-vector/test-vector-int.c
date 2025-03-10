b46eb54f2e4659da7e975ad641241f4fe5b50af3
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-03-10 14:20:55 +0300

    devel tests vector-int

--- task-2-vector/test-vector-int.c
+++ task-2-vector/test-vector-int.c
@@ -1,7 +1,11 @@
 #include "vector-int.h"
 #include "tests.h"
+#include <stdio.h>
 
 #define INIT_SIZE 10
+
+// TODO: ADD NEGATIVE TESTS
+// TODO: ADD AND ARRANGE INCLUDE SCOPES
 
 int main(int argc, char** argv) {
 
@@ -19,6 +23,11 @@ void VectorInt_testAppend() {
 	IVectorInt vector = initVectorInt(0);
 	
 	appendToVectorInt(vector, 10);
+
+	int* dataFromVector = getDataFromVectorInt(vector);
+	printf("%d\n", *dataFromVector);
+
+	disposeVectorInt(vector);
 }
 
 void Vectorint_testRemend() {
