40cc7453f204e7d3f91f03532fffe09ce04dcf63
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-03-31 09:11:29 +0300

    complete main function

--- task-4-segments/segments.cpp
+++ task-4-segments/segments.cpp
@@ -1,13 +1,33 @@
+/*
+*
+*	Task 4: Segments. 
+*	Created by GrindelfP on 2024-03-30.
+*
+*/
+
 #include "segments.h"
 #include <iostream>
-#include <map>
 
+/**
+* @brief Main function.
+* 
+* Main function of the program. Receives user input
+* of segments' vertexes. Calls functions to build
+* broiken lines out of the vertexes,
+*
+* @param argc number of main function parameters.
+* @param argv function parameters.
+*
+* @return 0 if worked as required. 
+*/
 int main(int argc, char** argv) {
 
-	std::map<int, int> segments;
+	std::vector<std::pair<int, int> > segments;
+	std::vector<std::vector<int> > brokenLine;
 	int 
 		numberOfSegments,
 		i,
+		j,
 		a, 
 		b;
 	
@@ -22,10 +42,57 @@ int main(int argc, char** argv) {
 				<< " and press ENTER after each of two vertexes:\n";
 		std::cin >> a;
 		std::cin >> b;
-		segments[a] = b;
+		segments.push_back(std::make_pair(a, b));
 	}
 	
+	// ====================
+	// BUILDING BROKEN LINE
+	// ====================
+	buildBrokenLine(segments, brokenLine);
+		
+
+	// ==================
+	// OUTPUT BROKEN LINE
+	// ==================
+	if (brokenLione.size()  == 0) std::cout << "There is no broken lines built!\n";
+	else {
+		for (i = 0; i < brokenLine.size(); ++i) {
+			std::cout << brokenLine.size() << std::endl;
+			std::cout << "Broken line "<< i+1 <<": ";
+			for (j = 0; j < brokenLine[i].size(); ++j) {
+				std::cout << brokenLine[i][j];
+				if (j != brokenLine[i].size() - 1) std::cout << ", ";
+			}
+			std::cout << std::endl;
+		}
+	}
 	
 	return 0;
+}
+
+
+/**
+* @brief Builds broken line.
+*
+* Uses provided segments list to build any possible 
+* beroken line out of this list. Broken line can be
+* cycled but cannot have branches (every node has
+* only two neighbours).
+*
+* @param segmetns list of segments (pairs of integers).
+* @param brokenLine list of broken lines.
+*
+* @return void.
+*/
+void buildBrokenLine(
+	const std::vector<std::pair<int, int> > segments, 
+	std::vector<std::vector<int> > brokenLine
+) { 
+	
+	for (std::pair<int, int> segment : segments) {
+		
+	}			
+
+	return;
 }
 
--- task-4-segments/segments.h
+++ task-4-segments/segments.h
@@ -1,14 +1,35 @@
 /**
-
-	Task 4: Segments. 
-	Created by GrindelfP on 2024-03-30.
-
+*
+*	Task 4: Segments. 
+*	Created by GrindelfP on 2024-03-30.
+*
 */
 
 #ifndef segments_h
 #define segments_h
 
+#include <vector>
+#include <utility>
 
+
+/**
+* @brief Builds broken line.
+*
+* Uses provided segments list to build any possible 
+* beroken line out of this list. Broken line can be
+* cycled but cannot have branches (every node has
+* only two neighbours).
+*
+* @param segmetns list of segments (pairs of integers).
+* @param brokenLine list of broken lines.
+*
+* @return void.
+*/
+void buildBrokenLine(
+	const std::vector<std::pair<int, int> > segments, 
+	std::vector<std::vector<int> > brokenLine
+); 
+	
 
 #endif // segments_h
 
