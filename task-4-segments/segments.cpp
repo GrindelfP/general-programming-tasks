320dba17a46ab0873367d341b47e0acf8e5036b3
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-03 21:42:15 +0300

    complete task 4

--- task-4-segments/segments.cpp
+++ task-4-segments/segments.cpp
@@ -2,8 +2,7 @@
 *
 *	Task 4: Segments. 
 *	Created by GrindelfP on 2024-03-30.
-*	
-* 	TODO 2025-03-31: not all broken lines are 'captured'
+*
 */
 
 #include "segments.h"
@@ -32,9 +31,9 @@ int main(int argc, char** argv) {
 		a, 
 		b;
 	
-	// =======================
-	// SEGMENTS INITIALIZATION
-	// =======================
+	// ======================= //
+	// SEGMENTS INITIALIZATION //
+	// ======================= //
 	std::cout << "\n====================\nBROKEN LINES BUILDER\n====================\n";
 	std::cout << "Please enter number of segments you want to create: ";
 	std::cin >> numberOfSegments;
@@ -47,15 +46,15 @@ int main(int argc, char** argv) {
 		segments.push_back(std::make_pair(a, b));
 	}
 	
-	// =====================
-	// BUILDING BROKEN LINES
-	// =====================
+	// ===================== //
+	// BUILDING BROKEN LINES //
+	// ===================== //
 	brokenLine = buildBrokenLine(segments);
 		
 
-	// ==================
-	// OUTPUT BROKEN LINE
-	// ==================
+	// ================== //
+	// OUTPUT BROKEN LINE //
+	// ================== //
 	std::cout << "==================================\n";
 	if (brokenLine.size()  == 0) std::cout << "There is no broken lines built!\n";
 	else {
@@ -72,16 +71,17 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-
 /**
 * @brief Builds broken line.
 *
 * Uses provided segments list to build any possible 
-* broken line out of this list. Broken line can be
-* cycled but cannot have branches (every node can occur
-* only in a singe broken line). Also broken lines cannot 
-* be formed by two vertices because in this case it will
-* not differ from a segment.
+* broken line out of this list. These lines set should
+* obey following rules:
+*
+* 1. Broken line vertex can have no more than 2 neighbours.
+* 2. A vertex can be included only in a single broken line.
+* 3. A cycle can be formed if broken line has simillar initial
+* and final vertices. 
 *
 * @param segmetns list of segments (pairs of integers).
 *
@@ -89,38 +89,98 @@ int main(int argc, char** argv) {
 */
 std::vector<std::vector<int> > buildBrokenLine(const std::vector<std::pair<int, int> > segments) { 
 
-	int 
-		a,
-		b,
-		i,
-		j,
-		last;
+	int i;
 	std::vector<int> line;
-	std::vector<std::vector<int> > brokenLine;
-	
-	for (i=0; i < segments.size(); ++i) {
-		if (a == b) break;
-		line.clear();
-		line.push_back(segments[i].first);
-		line.push_back(segments[i].second);
-		for (j=i; j < segments.size(); ++j) {
-			a = segments[j].first;
-			b = segments[j].second;
-			last = line.size()-1;
-			if (line[last] == a && line[0] != b) {
-				// STANDARD CASE (....2 + 2 3 -> ....2 3)
-				line.push_back(b);
-				j=i;
-			} else if (line[last] == a && line[0] == b) {
-				// CYCLE CASE (1 .... 3 + 3 1 -> 1 .... 3 1)
-				// cycles can only be formed with initial node
-				line.push_back(b);
-				break;
-			} 
+	std::vector<std::vector<int> > 
+				brokenLines,
+				brokenLinesFiltered;
+
+	// ====================== //
+	// BUILD BROKEN LINES SET //
+	// ====================== //
+	for(std::pair<int, int> segment : segments) {
+		std::pair<int, int> included = vertexIncluded(segment, brokenLines);
+		if (included.first == 1) {
+		// CASE: first of the pair is the end vertex  //
+			brokenLines[included.second].push_back(segment.second); 	
+		} else if (included.first == 2) {
+		// CASE: second of the pair is the end vertex //
+			brokenLines[included.second].push_back(segment.first); 	
+		} else if (included.first == 3) {
+		// CASE: cycle, i.e. first of pair is the start vertex // 
+		// and second is the end vertex 		       //		
+			brokenLines[included.second].push_back(segment.first); 	
+		} else if (included.first == 4) {
+		// CASE: cycle, i.e. second of pair is the start vertex // 
+		// and first is the end vertex 				//	
+			brokenLines[included.second].push_back(segment.second); 	
+		} else {
+		// CASE: if no vertices are included in any broken line //
+			std::vector<int> newLine = { segment.first, segment.second };
+			brokenLines.push_back(newLine);
 		}
-		if (line.size() > 2) brokenLine.push_back(line);	
 	}
 	
-	return brokenLine;
+	// ======================================= //
+	// FILTER LINES WITH LENGTHS <= 2 VERTICES //
+	// ======================================= //
+	for (i = 0; i < brokenLines.size(); ++i) {
+		if (brokenLines[i].size() > 2) 
+			brokenLinesFiltered.push_back(brokenLines[i]);
+	}
+		
+	return brokenLinesFiltered;
+}
+
+/**
+* @brief Finds ocurances of given segment vertices 
+*	in the set of broken lines.
+* 
+* Checks each of the broken lines and tries to find the
+* occurance of the given segment vertices in any of them.
+* If any occurance found it returns specific code and
+* the index of the broken line, which contain common vertex.
+* If no occurances were found it returns code 0 and index -1.
+* SPECIFIC CODES:
+*	0 - no occurances were found 
+*	1 - first vertex is the last in the broken line 
+*	2 - second vertex is the last in the broken line
+*	3 - first vertex is the broken line's start vertex
+*		and the first vertex is the line's end vertex.	  
+*	4 - second vertex is the broken line's start vertex
+*		and the first vertex is the line's end vertex.
+* Thus, the output is a pair <int, int>, e.g. (0, -1) or (3, 2).
+*
+* @param segment segment for which the occurance of its vertices should be found.
+* @param brokenLines broken lines which may contain vertices.
+* 
+* @return pair: code and index of the line.
+*/
+std::pair<int, int> vertexIncluded(
+	const std::pair<int, int> segment, 
+	const std::vector<std::vector<int> > brokenLines
+) {
+	int 
+		i,
+		code;
+	
+	for (i = 0; i < brokenLines.size(); ++i) {
+		std::vector<int> line = brokenLines[i];
+		int last = line.size() - 1;
+		if (line[0] == line[last]) break; // CASE OF CYCLE: this line cannot be updated
+		if (line[last] == segment.first) code = 1;
+		else if (line[last] == segment.second) code = 2;
+		else if (line[0] == segment.first && line[last] == segment.second) code = 3;
+		else if (line[0] == segment.second && line[last] == segment.first) code = 4;
+		else code = 0;
+
+		if (code != 0) break;
+		else if (i == brokenLines.size() - 1) {
+			i = -1;
+			break;	
+		}
+	}
+
+	return { code, i };
 }
 
--- task-4-segments/segments.h
+++ task-4-segments/segments.h
@@ -16,17 +16,49 @@
 * @brief Builds broken line.
 *
 * Uses provided segments list to build any possible 
-* broken line out of this list. Broken line can be
-* cycled but cannot have branches (every node can occur
-* only in a singe broken line). Also broken lines cannot 
-* be formed by two vertices because in this case it will
-* not differ from a segment.
+* broken line out of this list. These lines set should
+* obey following rules:
 *
-* @param segments segments to create broken lines.
+* 1. Broken line vertex can have no more than 2 neighbours.
+* 2. A vertex can be included only in a single broken line.
+* 3. A cycle can be formed if broken line has simillar initial
+* and final vertices. 
 *
-* @return void.
+* @param segmetns list of segments (pairs of integers).
+*
+* @return vector of broken lines.
 */
 std::vector<std::vector<int> > buildBrokenLine(const std::vector<std::pair<int, int> > segments); 
+
+
+/**
+* @brief Finds ocurances of given segment vertices 
+*	in the set of broken lines.
+* 
+* Checks each of the broken lines and tries to find the
+* occurance of the given segment vertices in any of them.
+* If any occurance found it returns specific code and
+* the index of the broken line, which contain common vertex.
+* If no occurances were found it returns code 0 and index -1.
+* SPECIFIC CODES:
+*	0 - no occurances were found 
+*	1 - first vertex is the last in the broken line 
+*	2 - second vertex is the last in the broken line
+*	3 - first vertex is the broken line's start vertex
+*		and the first vertex is the line's end vertex.	  
+*	4 - second vertex is the broken line's start vertex
+*		and the first vertex is the line's end vertex.
+* Thus, the output is a pair <int, int>, e.g. (0, -1) or (3, 2).
+*
+* @param segment segment for which the occurance of its vertices should be found.
+* @param brokenLines broken lines which may contain vertices.
+* 
+* @return pair: code and index of the line.
+*/
+std::pair<int, int> vertexIncluded(
+	const std::pair<int, int> segment, 
+	const std::vector<std::vector<int> > brokenLines
+);
 	
 #endif // segments_h
 
