a7caa0f8de4dcd801955a1fbc5ac80092dfe01e9
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-07 13:15:07 +0300

    create task 6

---
+++ task-6-unique/unique.cpp
@@ -0,0 +1,10 @@
+/*
+*
+*	Task 6: Unique algotythm. 
+*	Created by GrindelfP on 2024-04-07.
+*
+*/
+
+#include "unique.h"
+
+
---
+++ task-6-unique/unique.h
@@ -0,0 +1,13 @@
+/*
+*
+*	Task 6: Unique algotythm. 
+*	Created by GrindelfP on 2024-04-07.
+*
+*/
+
+#ifndef unique_h
+#define unique_h
+
+
+
+#endif // unique_h
