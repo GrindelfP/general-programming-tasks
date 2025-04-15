a4158445fd4855c4c2867476e54c4ec4ad9d4fbc
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-15 09:05:14 +0300

    update header file

--- task-3-collections/container-utils.h
+++ task-3-collections/container-utils.h
@@ -9,41 +9,41 @@
 
 /// @brief Removes specific values within given range.
 /// 
-/// @tparam T type of values in container.
-/// @tparam I iterator type.
+/// @tparam Type type of values in container.
+/// @tparam Iterator iterator type.
 /// 
 /// @param start lower bound of the range.
 /// @param fin upper bound of the range.
 /// @param value value to be removed
 /// 
 /// @return iterator for updated container.
-template<typename T, typename I>
-I dremove(I start, I fin, T value);
+template<typename Type, typename Iterator>
+Iterator dremove(Iterator start, Iterator fin, Type value);
 
 /// @brief 
 ///
-/// @tparam P predicate type.
-/// @tparam I iterator type. 
+/// @tparam Predicate predicate type.
+/// @tparam Iterator iterator type. 
 ///
 /// @param start lower bound of the range.
 /// @param fin upper bound of the range.
 /// @param pred predicate for condition.
 ///
 /// @return iterator for updated container.
-template<typename P, typename I>
-I dremoveIf(I start, I fin, P pred);
+template<typename Predicate, typename Iterator>
+Iterator dremoveIf(Iterator start, Iterator fin, Predicate pred);
 
 /// @brief 
 ///
-/// @tparam F function type.
-/// @tparam I iterator type.
+/// @tparam Function function type.
+/// @tparam Iterator iterator type.
 ///
 /// @param start lower bound of the range.
 /// @param fin upper bound of the range.
-/// @param fucnt function to transform the collection.
+/// @param fucntion function to transform the collection.
 ///
 /// @return iterator for updated container.
-template<typename I, typename F>
-I transform(I start, I fin, F fucnt);
+template<typename Iterator, typename Function>
+Iterator transform(Iterator start, Iterator fin, Function fucntion);
 
 #endif // container_utils
