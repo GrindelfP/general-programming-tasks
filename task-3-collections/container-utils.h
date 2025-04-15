21fcda4d04cebd504f15992ad0d2916dcb4ee2e4
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-15 08:40:10 +0300

    complete header file

--- task-3-collections/container-utils.h
+++ task-3-collections/container-utils.h
@@ -1,10 +1,49 @@
+/*
+*
+*	Task 3: General algorythms for containers in C++. 
+*	Created by GrindelfP on 2024-04-15.
+*
+*/
+#ifndef container_utils
+#define container_utils
+
+/// @brief Removes specific values within given range.
+/// 
+/// @tparam T type of values in container.
+/// @tparam I iterator type.
+/// 
+/// @param start lower bound of the range.
+/// @param fin upper bound of the range.
+/// @param value value to be removed
+/// 
+/// @return iterator for updated container.
 template<typename T, typename I>
 I dremove(I start, I fin, T value);
 
-
+/// @brief 
+///
+/// @tparam P predicate type.
+/// @tparam I iterator type. 
+///
+/// @param start lower bound of the range.
+/// @param fin upper bound of the range.
+/// @param pred predicate for condition.
+///
+/// @return iterator for updated container.
 template<typename P, typename I>
-I dremove(I start, I fin, P pred);
+I dremoveIf(I start, I fin, P pred);
 
+/// @brief 
+///
+/// @tparam F function type.
+/// @tparam I iterator type.
+///
+/// @param start lower bound of the range.
+/// @param fin upper bound of the range.
+/// @param fucnt function to transform the collection.
+///
+/// @return iterator for updated container.
 template<typename I, typename F>
-I dremove(I start, I fin, F fucnt);
+I transform(I start, I fin, F fucnt);
 
+#endif // container_utils
