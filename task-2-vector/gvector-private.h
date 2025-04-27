8e41ccccca1a422b689eeb7f57f49dcbeec6bc07
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-27 14:33:55 +0300

    [UPDATE] structure declaration

--- task-2-vector/gvector-private.h
+++ task-2-vector/gvector-private.h
@@ -13,3 +13,19 @@
     Created by GrindelfP on 2025-04-27. 
 
 */
+
+#ifndef gvector_private_h
+#define gvector_private_h
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct GVectorInternal {
+    void* data;          // Pointer to the dynamically allocated array
+    size_t size;         // Number of elements currently stored
+    size_t capacity;     // Total allocated space
+    size_t element_size; // Size of each element in bytes
+} GVectorInternal;
+
+#endif // gvector_private_h
