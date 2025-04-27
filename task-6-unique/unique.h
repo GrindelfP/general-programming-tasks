76b6e9838649aae085c2b4c58cd1cf59deaf7fdb
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-27 16:15:29 +0300

    [UPDATE] add unque2 function

--- task-6-unique/unique.h
+++ task-6-unique/unique.h
@@ -39,6 +39,32 @@ namespace guique {
         return itOut;
     }
 
+    /// @brief Creates set over container.
+    ///
+    /// Takes container with values and fills a new one 
+    /// (itOut is the iterator for the output container)
+    /// only with unique values out of range [it1, it2]. 
+    /// Works with unordered containers.
+    ///
+    /// @tparam InIt type of input container iterator.
+    /// @tparam OutIt type of output container iterator.
+    /// @param it1 iterator, left bound of the initial container.
+    /// @param it2 iterator, right bound of the initial container.
+    /// @param itOut iterator, output container.
+    /// @return iterator to the output container with unique values only.
+    template <typename InIt, typename OutIt>
+    OutIt unique2(InIt it1, InIt it2, OutIt itOut) {
+        std::unordered_set<typename std::iterator_traits<InIt>::value_type> seen;
+
+        for (; it1 != it2; ++it1) {
+            if (seen.find(*it1) == seen.end()) {
+                *itOut++ = *it1;
+                seen.insert(*it1);
+            }
+        }
+        return itOut;
+    }
+
 }
 
 #endif // unique_h
