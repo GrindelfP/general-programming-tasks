8d3b68cae20ab5122bf762a5f122b5ffb68658f0
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-27 14:31:10 +0300

    [UPDATE] add header comments

---
+++ task-2-vector/gvector-private.h
@@ -0,0 +1,15 @@
+/*
+
+   _____     __      __       _             
+  / ____|    \ \    / /      | |            
+ | |  __ _____\ \  / /__  ___| |_ ___  _ __ 
+ | | |_ |______\ \/ / _ \/ __| __/ _ \| '__|
+ | |__| |       \  /  __/ (__| || (_) | |   
+  \_____|        \/ \___|\___|\__\___/|_|   
+                                            
+                                            
+    GVector: a data structure for generic types. 
+    Private header file. NOT AVAILABLE IN USAGE.
+    Created by GrindelfP on 2025-04-27. 
+
+*/
---
+++ task-2-vector/test-gvector.c
@@ -0,0 +1,15 @@
+/*
+
+   _____     __      __       _             
+  / ____|    \ \    / /      | |            
+ | |  __ _____\ \  / /__  ___| |_ ___  _ __ 
+ | | |_ |______\ \/ / _ \/ __| __/ _ \| '__|
+ | |__| |       \  /  __/ (__| || (_) | |   
+  \_____|        \/ \___|\___|\__\___/|_|   
+                                            
+                                            
+    GVector: a data structure for generic types. 
+    Functionality test file.
+    Created by GrindelfP on 2025-04-27. 
+
+*/
