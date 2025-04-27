f5ec606971fae9aaa6db26e7177cbc4778c861ea
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-28 02:20:12 +0300

    [UNSTABLE] something wrong happens after set 1

--- task-8-predecessors/preds.cpp
+++ task-8-predecessors/preds.cpp
@@ -15,6 +15,7 @@
 
 #define SKILLS_PATH "civ2.csv"
 #define SKILLS_SETS_PATH "civ2-ordered.txt"
+#define NODEP "nil"
 
 std::vector<Skill> readSkillsFromCSV(const std::string& filename) {
     std::vector<Skill> skills;
@@ -49,8 +50,54 @@ std::vector<Skill> readSkillsFromCSV(const std::string& filename) {
 
 std::vector<std::vector<Skill> > buildSets(const std::vector<Skill> &skillList) {
 
+    std::vector<std::vector<Skill> > skillSets;
+    std::vector<Skill> localList = skillList;
+    std::vector<Skill> localListTmp = skillList;
+    int counter = 0;
     
+    // SET 0 //
+    std::vector<Skill> set0;
+    for (int i = 0; i < localList.size(); ++i) {
+        if (localList[i].dep1 == NODEP && localList[i].dep2 == NODEP) {
+            set0.push_back(localList[i]);
+            counter++;
+        }
+    }
+    skillSets.push_back(set0);
 
+    // SET 1... //
+    while (counter < skillList.size()) {
+        std::vector<Skill> seti;
+        int lastSetIndex = skillSets.size() - 1;
+        std::vector<Skill> lastSet = skillSets[lastSetIndex];
+        for (int i = 0; i < localList.size(); ++i) {
+            for (int j = 0; j < lastSet.size(); ++j) {
+                // 1. Has 1 dep 
+                if (localList[i].dep1 == NODEP && localList[i].dep2 == lastSet[j].code) {
+                    seti.push_back(localList[i]);
+                    counter++;
+                } else if (localList[i].dep1 == lastSet[j].code && localList[i].dep2 == NODEP) {
+                    seti.push_back(localList[i]);
+                    counter++;
+                } else if (localList[i].dep1 != NODEP && localList[i].dep2 != NODEP) { // 2. Has 2 deps
+                    if (lastSetIndex == 0) {
+                        if (localList[i].dep1 == lastSet[j].code && localList[i].dep2 == lastSet[j].code) {
+                            seti.push_back(localList[i]);
+                            counter++;
+                        } 
+                    } else {
+                        if (localList[i].dep1 == lastSet[j].code || localList[i].dep2 == lastSet[j].code) {
+                            seti.push_back(localList[i]);
+                            counter++;
+                        } 
+                    }
+                }                
+            }   
+        }
+        skillSets.push_back(seti);
+    } 
+
+    return skillSets;
 }
 
 void writeSkillSets(const std::vector<std::vector<Skill> > &skillSets, const std::string &path) {
@@ -63,14 +110,14 @@ void writeSkillSets(const std::vector<std::vector<Skill> > &skillSets, const std
             outputFile << skill << std::endl;
         }
         outputFile << std::endl;
+        number++;
     }
 
 }
 
 int main() {
     std::vector<Skill> skillList = readSkillsFromCSV(SKILLS_PATH);
-    // std::vector<std::vector<Skill> > skillSets = buildSets(skillList);
-    std::vector<std::vector<Skill> > skillSets = {skillList};
+    std::vector<std::vector<Skill> > skillSets = buildSets(skillList);
     writeSkillSets(skillSets, SKILLS_SETS_PATH);
 
     return 0;
