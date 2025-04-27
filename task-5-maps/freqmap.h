e7b5b72b9cc556f3257a3505c9d280760a75f75a
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-04-27 15:14:02 +0300

    [UPDATE] create base files

---
+++ task-5-maps/freqmap.h
@@ -0,0 +1,7 @@
+/*
+
+    Task 5. Frequency dictionary.
+    Header file containing getWordFrequency() function.
+    Created by GrindelfP on 2024-04-27.
+
+*/
---
+++ task-5-maps/test-freqmap.c
@@ -0,0 +1,7 @@
+/*
+
+    Task 5. Frequency dictionary.
+    Test file.
+    Created by GrindelfP on 2024-04-27.
+
+*/
